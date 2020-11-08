ced556f24ea2102ca584d99ebce4ab92d62082b1
gamerdude333 <65493032+gamerdude333@users.noreply.github.com>
2020-11-08 20:35:35 +0800

    Added help command and changed all commands to embed

--- team-azuki/bot.py
+++ team-azuki/bot.py
@@ -3,11 +3,11 @@ import os
 import signal
 import threading
 from datetime import datetime
-
+from hata import Embed, ReuAsyncIO, KOKORO, Client
 import dotenv
 import hata
-
 from botdata import BotData
+from hata.discord.guild import GuildPreview
 
 dotenv.load_dotenv()
 
@@ -26,20 +26,61 @@ async def message_create(client: hata.discord.Client, message: hata.discord.Mess
         return
 
     elif message.content == '.ping':
-        await client.message_create(message.channel, f'pong! ({(datetime.now() - message.created_at).seconds // 1000}ms)')
+        embed = Embed(
+         'Pong!',
+         f'({(datetime.now() - message.created_at).seconds // 1000}ms)',
+         color=0x00fbff
+         )
+        await client.message_create(message.channel, embed=embed)
+
+    elif message.content == '.help':
+        embed = Embed(
+         'List Of Commands',
+         '.ping\n.owners\n.set\n.set\n.get\n.stop',
+         color=0x00fbff
+         )
+        await client.message_create(message.channel, embed=embed)
+
+    elif message.content == '.owners':
+        embed = Embed(
+         'Owners',
+         f'{", ".join(u.full_name for u in azuki.owners)}',
+         color=0x00fbff
+         )
+        await client.message_create(message.channel, embed=embed)
     
     elif message.content.startswith('.set '):
         args = message.content.split()[1:]
         if len(args) != 2:
-            return await client.message_create(message.channel, 'Command `set` accepts 2 arguments')
+            embed = Embed(
+             'Error',
+             'Command `set` accepts 2 arguments',
+             color=0xff0000
+            )
+            return await client.message_create(message.channel, embed=embed)
         data[args[0]] = args[1]
-        await client.message_create(message.channel, f'`"{args[0]}": "{args[1]}"`')
+        embed = Embed(
+         'Success',
+         f'`"{args[0]}" has been set as "{data[args[0]]}"`',
+         color=0x00fbff
+        )
+        await client.message_create(message.channel, embed=embed)
     
     elif message.content.startswith('.get '):
         args = message.content.split()[1:]
         if len(args) != 1:
-            return await client.message_create(message.channel, 'Command `get` accepts 1 argument')
-        await client.message_create(message.channel, f'`"{args[0]}": "{data[args[0]]}"`')
+            embed = Embed(
+             'Error',
+             'Command `get` accepts 1 argument',
+             color=0xff0000
+            )
+            return await client.message_create(message.channel, embed=embed)
+        embed = Embed(
+         'Value Retrieved',
+         f'`"{args[0]}" is equal to  "{data[args[0]]}"`',
+         color=0x00fbff
+        )
+        await client.message_create(message.channel, embed=embed)
 
     elif message.content == '.stop':
         data.save()
@@ -50,6 +91,14 @@ async def message_create(client: hata.discord.Client, message: hata.discord.Mess
         client.loop.stop()
         thread_id = threading.main_thread().ident
         os.kill(thread_id, signal.SIGKILL)
+    
+    elif message.content.startswith('.'):
+        embed = Embed(
+         'Invalid command',
+         'try using: `.help`',
+         color=0xff0000,
+            )
+        message = await client.message_create(message.channel,embed=embed)
 
 
 azuki.start()
